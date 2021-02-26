8c079b77c3889fa703f628b76ada361d57340b6c
Seonae <seonaebyeon1@gmail.com>
2021-02-26 15:30:44 -0500

    encrypt password error : The data argument must be of type string

---
+++ server/db/models/users.js
@@ -0,0 +1,68 @@
+// define your model here and establish assocaiton (in /db/index.js)
+const crypto = require('crypto')
+const Sequelize = require('sequelize')
+const db = require('../database')
+
+const User = db.define('user', {
+    email: {
+        type: Sequelize.STRING,
+        unique: true,
+        allownULL: false,
+        validate: {
+            isEmail: true
+        }
+    },
+    password: {
+        type:Sequelize.STRING,
+        // Making `.password` act like a func hides it when serializing to JSON.
+         // This is a hack to get around Sequelize's lack of a "private" option.
+         get() {
+             return ()=> this.getDataValue('password')
+         }
+    },
+    salt: {
+        type: Sequelize.STRING,
+        get() {
+            return () => this.getDataValue
+        }
+    }
+})
+module.exports = User
+
+/**
+ * instanceMethods
+ */
+User.prototype.correctPassword = function(candidatePwd) {
+  return User.encryptPassword(candidatePwd, this.salt()) === this.password()
+}
+
+/**
+ * classMethods
+ */
+User.generateSalt = function() {
+  return crypto.randomBytes(16).toString('base64')
+}
+
+User.encryptPassword = function(plainText, salt) {
+  return crypto
+    .createHash('RSA-SHA256')
+    .update(plainText)
+    .update(salt)
+    .digest('hex')
+}
+
+/**
+ * hooks
+ */
+// const setSaltAndPassword = user => {
+//   if (user.changed('password')) {
+//     user.salt = User.generateSalt()
+//     user.password = User.encryptPassword(user.password(), user.salt())
+//   }
+// }
+
+// User.beforeCreate(setSaltAndPassword)
+// User.beforeUpdate(setSaltAndPassword)
+// User.beforeBulkCreate(users => {
+//   users.forEach(setSaltAndPassword)
+// })
